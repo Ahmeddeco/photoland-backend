b308a6a46e03352b2be245f5ddecbf537ab19127
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-03 22:06:49 +0300

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductModule } from './product/product.module'
+import { Product } from './product/entities/product.entity'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
